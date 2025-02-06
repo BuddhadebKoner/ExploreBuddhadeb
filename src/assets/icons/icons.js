6214d36e4cca9f502faa0b59a4fc0514fce09287
BuddhadebKoner <jeetkoner36@gmail.com>
2025-02-06 16:29:46 +0530

    seo make better -->

--- src/assets/icons/icons.js
+++ src/assets/icons/icons.js
@@ -16,8 +16,8 @@ import hamburger from './menu_hamburgur.svg'
 import close from './close.svg';
 import githubDark from './githubDark.png';
 
-const heroBanner = 'https://i.ibb.co/vBQxxn7/cartoon-man-wearing-glasses.jpg'
-const profile = 'https://i.ibb.co/56hzHJn/profile.jpg'
+const heroBanner = 'https://cloud.appwrite.io/v1/storage/buckets/66f8e10b0034b56b85be/files/67a494d9000dcc0ff37b/view?project=66f8cb12003c2ead11e2&'
+const profile = 'https://cloud.appwrite.io/v1/storage/buckets/66f8e10b0034b56b85be/files/67a494df0002ff6d99ae/view?project=66f8cb12003c2ead11e2&'
 
 
 const assets = {
